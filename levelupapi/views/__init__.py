a1755ca36eb2c9f04cdd831c4431cd6144fd915d
Joshua Medlen <joshua246@gmail.com>
2020-12-05 15:39:28 -0600

    profiles is WIP. added router. profile viewset

--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -3,3 +3,4 @@ from .auth import register_user
 from .gametype import GameTypes, GameTypeSerializer
 from .game import Games, GameSerializer
 from .event import Events, GamerEventSerializer, GameSerializer, EventSerializer, EventUserSerializer
+from .profile import Profile
